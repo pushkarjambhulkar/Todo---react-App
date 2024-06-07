43390a0d3def270b51b31c84699c2dd89ac4b1a1
pushkarjambhulkar <pushkarjambhulkar200@gmail.com>
2024-06-07 11:02:00 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todolist, setTodolist] = useState([]);
+
+  const saveToDoList = (event) => {
+    event.preventDefault();
+    
+    const toname = event.target.todo.value;
+    if (!todolist.includes(toname)) {
+      const finalDolist = [...todolist, toname];
+      setTodolist(finalDolist);
+    } else {
+      alert("ToDo Name Already exists...");
+    }
+
+    event.target.todo.value = "";  // Clear the input field after submission
+  };
+
+  const deleteToDo = (index) => {
+    const newTodolist = todolist.filter((_, i) => i !== index);
+    setTodolist(newTodolist);
+  };
+
+  const List = todolist.map((value, index) => {
+    return (
+      <ToDoListItems
+        value={value}
+        key={index}
+        indexNumber={index}
+        deleteToDo={deleteToDo}
+      />
+    );
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <center>
+        <h1>ToDo List</h1>
+      </center>
+      
+      <form onSubmit={saveToDoList}>
+        <input type="text" name="todo" placeholder="Enter a task" />
+        <button type="submit">Save</button>
+      </form>
+      <div className='outerDiv'>
+        <ul>
+          {List}
+        </ul>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+function ToDoListItems({ value, indexNumber, deleteToDo }) {
+  let [status,setStatus]=useState(false)
+  let checkStatus=()=>{
+    setStatus(!status)
+  }
+  return (
+  
+    <li className={(status)? 'completetodo':''} onClick={checkStatus}>
+      {value}
+      <span onClick={() => deleteToDo(indexNumber)} style={{ cursor: 'pointer', color: 'red', marginLeft: '10px' }}>
+        &times;
+      </span>
+    </li>
+  );
+}
